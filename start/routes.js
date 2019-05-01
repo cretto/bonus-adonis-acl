ec944c0f7f20a74c3ed111e8743b166eca60a735
Henrique Pereira <hpereira@vistajet.com>
2019-05-01 18:24:48 -0300

    ACL com adonis concluido

--- start/routes.js
+++ start/routes.js
@@ -19,7 +19,27 @@ Route.post('/sessions', 'SessionController.store')
 
 Route.post('/users', 'UserController.store')
 Route.put('/users/:id', 'UserController.update').middleware('auth')
+Route.get('/users', 'UserController.index')
 
 Route.resource('/posts', 'PostController')
+  .apiOnly()
+  .except(['index', 'show'])
+  .middleware(['auth', 'is:(administrator || moderator)'])
+
+Route.get('/posts', 'PostController.index').middleware([
+  'auth',
+  'can:(read_posts || read_private_posts)'
+])
+
+Route.get('/posts/:id', 'PostController.show').middleware([
+  'auth',
+  'can:(read_posts || read_private_posts)'
+])
+
+Route.resource('/permissions', 'PermissionController')
+  .apiOnly()
+  .middleware('auth')
+
+Route.resource('/roles', 'RoleController')
   .apiOnly()
   .middleware('auth')
